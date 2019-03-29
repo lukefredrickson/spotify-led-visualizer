ed61d43b8bc67695b165bbba88f871d5a5ecc1a3
Luke Fredrickson <lukeefredrickson@gmail.com>
2019-03-28 21:06:49 -0400

    realized express app generator was overengineering

--- authorization_code/app.js
+++ authorization_code/app.js
@@ -14,7 +14,7 @@ var querystring = require("querystring");
 var cookieParser = require("cookie-parser");
 
 var client_id = "35aff29158f344858037d41be2493582"; // Your client id
-var client_secret = "88721945935245449524fd71199a4fe4"; // Your secret
+var client_secret = "772b8d700e10495590f046da6c975e78"; // Your secret
 var redirect_uri = "http://localhost:8888/callback"; // Your redirect uri
 
 /**
--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-var express = require("express"); // Express web server framework
-var request = require("request"); // "Request" library
+var express = require("express");
+var request = require("request");
 var cors = require("cors");
 var querystring = require("querystring");
 var cookieParser = require("cookie-parser");
 
-var client_id = "35aff29158f344858037d41be2493582"; // Your client id
-var client_secret = "772b8d700e10495590f046da6c975e78"; // Your secret
-var redirect_uri = "http://localhost:8888/callback"; // Your redirect uri
+var clientId = "35aff29158f344858037d41be2493582"; // Your client id
+var clientSecret = require("./keys.json").spotify_client_secret; // Your secret
+var redirectUri = "http://localhost:8888/callback"; // Your redirect uri
 
 var stateKey = "spotify_auth_state";
 
